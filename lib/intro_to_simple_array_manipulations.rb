06e7d6a27cad0ae78d41eeca715ae51a59caef2e
Jonah Offitzer <joffitzer@gmail.com>
2019-10-16 00:49:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,4 +38,31 @@ def using_uniq(haircuts)
   new_array = haircuts.uniq
 end
 
+def using_flatten(instruments)
+  flat_array = 
+end
 
+describe "using_flatten" do 
+  it "takes in an argument of an array that contains other arrays and uses the flatten method to return an array of strings" do 
+     instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
+     flat_array = using_flatten(instruments)
+     expect(flat_array).to eq(["Saxophone", "Piano", "Trumpet", "Violin", "Drums", "Flute"])
+  end
+end
+
+
+describe "using_delete" do 
+  it "takes in two arguments, an array and a string, and uses the delete method to remove any items from the array that are equal to that string" do
+    instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+    no_offense_steven = using_delete(instructors, "Steven")
+    expect(instructors).not_to include("Steven")
+  end
+end
+
+describe "using_delete_at" do 
+  it "takes in two arguments, an array and an integer and deletes the element at the index of the array that is equal to that integer" do 
+    famous_robots = ["Johnny 5", "R2D2", "Robocop"]
+    deleted_robot = using_delete_at(famous_robots, 2)
+    expect(deleted_robot).to eq("Robocop")
+  end
+end
