67fdae50a725050acb75d890bc00e9160edf7318
Jonah Offitzer <joffitzer@gmail.com>
2019-10-16 00:48:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -34,8 +34,8 @@ def using_insert(list_of_programming_languages, another_language)
   new_array = list_of_programming_languages.insert(4, another_language)
 end
 
-def using_uniq
-  
+def using_uniq(haircuts)
+  new_array = haircuts.uniq
 end
 
 
