ea88fd6aafdd0d7d377f993bce73c1fd6d60b333
Jonah Offitzer <joffitzer@gmail.com>
2019-10-16 00:42:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,6 +30,6 @@ def using_concat(my_favorite_things, more_favs)
   all_my_favs = my_favorite_things.concat(more_favs)
 end
 
-def using_insert(list_of_programming_languages, another_language)
-  list_of_programming_languages.insert(another_language)
+def using_insert(new_array)
+  new_array = list_of_programming_languages.insert(another_language)
 end
