b9b41193e304b08710995d75a91eae84f18a0960
Jonah Offitzer <joffitzer@gmail.com>
2019-10-16 00:54:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,10 +42,10 @@ def using_flatten(instruments)
   flat_array = instruments.flatten
 end
 
-def using_delete(instructors)
+def using_delete(instructors, no_offense_steven)
   no_offense_steven = instructors.delete("Steven")
 end
 
-def using_delete_at(famous_robots)
+def using_delete_at(famous_robots, deleted_robot)
   deleted_robot = famous_robots.delete_at(2)
 end
